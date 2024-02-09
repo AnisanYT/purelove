2c118594c38ab21f4769b9e43585fcaa57f2de33
AnisanYT <mejiadani07@gmail.com>
2024-02-08 22:08:30 -0600

    commitTest

---
+++ JavaScript/script.js
@@ -0,0 +1,64 @@
+function abrirVentana() {
+    var overlay = document.getElementById('overlay');
+    var ventanaEmergente = document.getElementById('ventanaEmergente');
+
+    overlay.style.display = 'block';
+    ventanaEmergente.style.display = 'block';
+}
+
+function cerrarVentana() {
+    var overlay = document.getElementById('overlay');
+    var ventanaEmergente = document.getElementById('ventanaEmergente');
+
+    overlay.style.display = 'none';
+    ventanaEmergente.style.display = 'none';
+}
+
+
+function abrirVentana2() {
+    var overlay = document.getElementById('overlay2');
+    var ventanaEmergente = document.getElementById('ventanaEmergente2');
+
+    overlay.style.display = 'block';
+    ventanaEmergente.style.display = 'block';
+}
+
+function cerrarVentana2() {
+    var overlay = document.getElementById('overlay2');
+    var ventanaEmergente = document.getElementById('ventanaEmergente2');
+
+    overlay.style.display = 'none';
+    ventanaEmergente.style.display = 'none';
+}
+
+function abrirVentana3() {
+    var overlay = document.getElementById('overlay3');
+    var ventanaEmergente = document.getElementById('ventanaEmergente3');
+
+    overlay.style.display = 'block';
+    ventanaEmergente.style.display = 'block';
+}
+
+function cerrarVentana3() {
+    var overlay = document.getElementById('overlay3');
+    var ventanaEmergente = document.getElementById('ventanaEmergente3');
+
+    overlay.style.display = 'none';
+    ventanaEmergente.style.display = 'none';
+}
+
+function abrirVentana4() {
+    var overlay = document.getElementById('overlay4');
+    var ventanaEmergente = document.getElementById('ventanaEmergente4');
+
+    overlay.style.display = 'block';
+    ventanaEmergente.style.display = 'block';
+}
+
+function cerrarVentana4() {
+    var overlay = document.getElementById('overlay4');
+    var ventanaEmergente = document.getElementById('ventanaEmergente4');
+
+    overlay.style.display = 'none';
+    ventanaEmergente.style.display = 'none';
+}
